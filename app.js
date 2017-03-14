a9bc59cdb9cb03ecde951b38f42b2404d9e22c3e
mbolanosc <mrybc96@gmail.com>
2017-03-13 21:53:13 -0600

    final commit 4 today.

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ window.addEventListener('load', init, false);
 function init() {
   console.log('init');
   //var element = $("#mainsvg");
+  //first text from left 2 right
   TweenLite.to("#top-text", 1.5, {
     x: function(index) {
       console.log(index);
-      return index + 0.3 * 100 // 100, 200, 300
+      return index + 0.3 * 85 // 100, 200, 300
     }
   });
+	//underscore display
 	TweenLite.to("#underscore", 2, {opacity:0, display:"none"});
+	//second text from right 2 left
+	TweenLite.to("#second-text", 1.5, {
+    x: function(index) {
+      console.log(index);
+      return index - 0.2 * 75 // 100, 200, 300
+    }
+  });
+
 
 
 }
